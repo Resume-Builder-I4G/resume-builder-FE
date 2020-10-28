0475951710736d89253aca77d119f62cd7a92c33
Umar Muhammad Zakari <Umarfarouqft@gmail.com>
2020-10-28 19:11:39 +0100

    corrected about page styling

--- src/screens/homepage/Home.js
+++ src/screens/homepage/Home.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
 
 function Home() {
-    return (
-      <div className="homepage">
-        <Navbar />
-      </div>
-    );
+  return (
+    <div className="homepage">
+      <Navbar />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
