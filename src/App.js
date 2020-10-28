051138517fac68868b9a23da2e0d2aedda17cdd5
Umar Muhammad Zakari <Umarfarouqft@gmail.com>
2020-10-28 16:21:50 +0100

    Add react router and implemented 404

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 // import About from "./screens/about/About";
 // // import "./bootstrap/dist/css/bootstrap.min.css";
@@ -9,9 +10,15 @@ import Error404 from "./screens/error404/Error404";
 function App() {
   return (
     <div className="App">
+      <Switch>
+        {/* <Route path="/" component={Home} /> */}
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Error404} />
+      </Switch>
       {/* <About /> */}
       {/* <Contact /> */}
-      <Error404 />
+      {/* <Error404 /> */}
       {/* <Footer /> */}
       {/* <GlobalFonts /> */}
       {/* <Success /> */}
