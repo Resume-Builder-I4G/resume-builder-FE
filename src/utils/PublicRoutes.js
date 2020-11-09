b5a92952c602c08e31a8d08ff03d6330b0e526c2
Umar Muhammad Zakari <Umarfarouqft@gmail.com>
2020-11-09 17:33:46 +0100

    Fixed Routing Issues

--- src/utils/PublicRoutes.js
+++ src/utils/PublicRoutes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { getToken } from "./utils/Common";
+import { getToken } from "./Common";
 
 // handle the public routes
 function PublicRoute({ component: Component, ...rest }) {
